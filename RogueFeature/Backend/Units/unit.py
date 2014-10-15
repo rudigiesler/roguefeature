e8b7d29f1ed78476eeb0d2723ea2095d5880da83
Rudi Giesler <rudi@praekelt.com>
2014-10-15 20:52:09 +0200

    Added basic unit class

---
+++ RogueFeature/Backend/Units/unit.py
@@ -0,0 +1,10 @@
+class Unit:
+    def __init__(self, x, y, imagePath, face, name, passable):
+        self.x = x
+        self.y = y
+        self.imgPath = imagePath
+        self.dir = face
+        self.passable = passable
+
+    def SetPoint(p):
+        self._point = p
