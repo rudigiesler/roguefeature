9b1af59bc34475b85aa50a2145fd1624c7f64b2f
Rudi Giesler <rudi@praekelt.com>
2014-10-17 04:49:10 +0200

    Finished Map class

--- RogueFeature/Backend/map.py
+++ RogueFeature/Backend/map.py
@@ -1,4 +1,5 @@
-import Point
+from .point import Point
+
 
 class Map:
 
@@ -18,3 +19,68 @@ class Map:
         self._rows = rows
         self._columns = columns
         self._name = name
+        self._points = [[None for i in range(rows)] for j in range(columns)]
+
+    def InitPoint(self, x, y, imagePath, face, passable):
+        try:
+            self._points[x][y] = Point(self, imagePath, face, passable)
+        except IndexError:
+            raise IndexError(
+                'Attempted to initialise a point outside the bounds of map')
+
+    def SetSpawn(self, x, y):
+        self._spawnX = x
+        self._spawnY = y
+
+    def AddPlayerToSpawn(self):
+        try:
+            # TODO: Create PC in Core
+            self._points[self._spawnX][self._spawnY].AddUnit(None)
+            # Core.pc.SetPoint(self._points[_spawnX][_spawnY])
+            # Core.pc.NewMap(self._spawnX, self._spawnY)
+        except AttributeError:
+            raise AttributeError('Spawn point for player not set')
+
+    def AddUnitToPoint(self, x, y, u):
+        try:
+            self._points[x][y].AddUnit(u)
+        except (AttributeError, IndexError) as e:
+            if isinstance(e, AttributeError):
+                raise AttributeError(
+                    'Point %d;%d is not initialised' % (x, y))
+            else:
+                raise IndexError(
+                    'Attempted to add unit to a point outside the bounds of ' +
+                    'map')
+
+    def BoundCheck(self, x, y):
+        return not x >= self._columns or x < 0 or y >= self._rows or y < 0
+
+    def Passable(self, x, y):
+        if not self.BoundCheck(x, y):
+            return False
+        p = self._points[x][y]
+        if p.passable:
+            for u in p.UnitList():
+                if not u.passable:
+                    return False
+        else:
+            return False
+        return True
+
+    def GetUnits(self, x, y):
+        try:
+            return self._points[x][y].UnitList()
+        except IndexError:
+            raise IndexError(
+                'Attempted to retrieve a unit list from a point outside ' +
+                'bounds of map.')
+
+    def Occupied(self, x, y):
+        return self._points[x][y].Occupied
+
+    def GetObjects(self, x, y):
+        return self._points[x][y].GetObjects()
+
+    def GetMobile(self, x, y):
+        return self._points[x][y].GetMobile()
